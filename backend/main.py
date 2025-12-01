e98e11ff53ea518415e028bee431614a3a4fc875
Sujan Subedi <officialsubedi51@gmail.com>
2025-12-01 09:40:55 +0545

    Day 2: Added student data structures using Python

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,25 @@
-# Backend entry point (will be used later)
 
+# Full student profile using dictionary
+
+student_1 = {
+    "name": "Sujan",
+    "marks": 72,
+    "interest": "Technology"
+}
+
+student_2 = {
+    "name": "Riya",
+    "marks": 85,
+    "interest": "Medical"
+}
+
+student_3 = {
+    "name": "Aayush",
+    "marks": 60,
+    "interest": "Business"
+}
+
+print("Student Profiles:")
+print(student_1)
+print(student_2)
+print(student_3)
